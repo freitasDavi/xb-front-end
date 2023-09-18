296520eb86a9162ea218c98df42146535bffdfa5
Davi <davi.frrs@outlook.com>
2023-09-18 14:06:30 -0300

    feat: Created components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script setup>
+import XTextfield from "@/components/XTextfield.vue";
+import XButton from "@/components/XButton.vue";
+import XPost from "@/components/XPost.vue";
 </script>
 
 <template>
@@ -9,6 +12,23 @@
         Esse é um twitter descentralizado, conecte sua carteira blockchain e use seus Etherums para enviar uma mensagem.
         Cada post enviado, você terá chance de ganhar um valor de Etherum de volta
       </p>
+      <XButton text="Conectar carteira" />
+      <XTextfield label="Post" name="post" class="mb-2" type="text" id="post" placeholder="John Doe" required />
+      <XButton text="Enviar post" />
+      <div class="flex items-center">
+        <span
+          class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900 h-fit">
+          Conectado!
+        </span>
+        <span class="truncate">
+          <!-- {{ currentAccount }} -->
+        </span>
+      </div>
+    </div>
+    <div class="mt-8 rounded-md border border-gray-700 text-white bg-gray-800 p-6 mx-auto w-full max-w-[600px]">
+      <h1 class="text-white text-lg mb-4">Todos os posts</h1>
+      <div class="text-center mb-4">Carregando...</div>
+      <XPost address="123" timestamp="16:54" message="Descrição da mensagem" />
     </div>
   </main>
 </template>
